779987fcdbcafb201a202c6f11d820ca64d33bdc
jonas reynders <reynders@live.be>
2018-04-28 11:10:45 +0200

    fixed issue with not using cors request

--- apiserver/api/controllers/FileSystemController.js
+++ apiserver/api/controllers/FileSystemController.js
@@ -1,16 +1,29 @@
 const fs = require('fs')
 const filefolder = './api/testfiles/'
+const path = require('path')
 
 exports.gettestfiles = function(req, res){
     var filescontainer = [];
     var dirlist = fs.readdirSync(filefolder)
     var filecontent;
     dirlist.forEach(function(filename) {
-        filecontent = fs.readFileSync(filefolder + filename, 'utf-8')
-        filescontainer.push({
-            "filename": filename,
-            "filecontent": JSON.parse(filecontent)
-        })
+        var ext = path.extname(filename)
+        if (ext === '.json') {
+            filecontent = fs.readFileSync(filefolder + filename, 'utf-8')
+            filescontainer.push({
+                "filename": filename,
+                "fileext": ext,
+                "filecontent": JSON.parse(filecontent)
+            })
+        }
+        if (ext === '.log'){
+            filecontent = fs.readFileSync(filefolder + filename, 'utf-8')
+            filescontainer.push({
+                "filename": filename,
+                "fileext": ext,
+                "filecontent": filecontent
+            })
+        }
     })
     res.json({filescontainer})
 }
--- apiserver/api/routes/FileSystemRoutes.js
+++ apiserver/api/routes/FileSystemRoutes.js
@@ -1,7 +1,8 @@
+var cors = require('cors')
+
 module.exports = function(app){
     var fileserver = require('../controllers/FileSystemController')
 
     //routes
-    app.route('/gettestfiles')
-        .get(fileserver.gettestfiles);
+    app.get('/gettestfiles', cors(), fileserver.gettestfiles);
 }
