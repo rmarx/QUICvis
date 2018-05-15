c2618dc639ae6e448f540a1b4e12f91a4cbee983
jonas reynders <reynders@live.be>
2018-05-15 10:38:00 +0200

    Changed layout of loaded files

--- visualisation-tool/src/components/generalsettings/FileSettings.vue
+++ visualisation-tool/src/components/generalsettings/FileSettings.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="filesettings" class="container w-50 float-left">
-        <div v-for="(file, fileindex) in traces" class="card card-body w-50">
+        <div v-for="(file, fileindex) in traces" class="container border w-25 float-left">
             <div v-bind:id="file.filename + '-span'">
             <button v-bind:id="file.filename + '-close'" class="btn btn-danger btn-sm" @click="removeFile(file.fileindex)">x</button>
             <button class="btn btn-light" type="button" data-toggle="collapse" v-bind:data-target="'#' + file.filename + '-conns'" aria-expanded="false" v-bind:aria-controls="file.filename + '-conns'">
                 {{ file.filename}}
             </button>
             <div v-bind:id="file.filename + '-conns'" v-bind:aria-labelledby="file.filename + '-conns'" v-bind:data-parent="file.filename + '-span'">
-                <div class="card card-body">
+                <div class="container border">
                     <div v-for="(conn, connindex) in file.conns">
                         <input type="checkbox" class="checkbox" v-bind:name="file.filename + '-conn' + connindex" :checked="!conn" @click="filterConn(fileindex, connindex)">
                         {{ 'conn' + (connindex + 1) }}
