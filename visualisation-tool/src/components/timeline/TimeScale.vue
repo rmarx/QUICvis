7642fdb9ab81681bc8b125a0d97ef92cb53388f0
jonas reynders <reynders@live.be>
2018-06-04 15:54:20 +0200

    Added scroll to zoom

--- visualisation-tool/src/components/timeline/TimeScale.vue
+++ visualisation-tool/src/components/timeline/TimeScale.vue
@@ -3,6 +3,7 @@
 
 <script lang="ts">
 import * as d3 from 'd3';
+import { svg } from 'd3';
 export default {
   name: "timescale",
   mounted() {
@@ -15,9 +16,18 @@ export default {
     let xscale = d3.scaleLinear().range([0, width]).domain([0,100])
     let axis = d3.axisBottom(xscale)
 
-    svgcont.append("g")
-      .call(axis)
+    let zoom = d3.zoom()
+      .scaleExtent([1, 40])
+      .translateExtent([[-100, -100], [width + 90, 500]])
+      .on('zoom', zoomed)
 
+    let gx = svgcont.append("g")
+      .call(axis)
+    svgcont.call(zoom)
+
+    function zoomed () {
+      gx.call(axis.scale(d3.event.transform.rescaleX(xscale)))
+    }
   }
 }
 </script>
