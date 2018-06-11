8bfaa1d0b79e50814d36525bb564ee015f14bcb0
jonas reynders <reynders@live.be>
2018-06-11 11:16:48 +0200

    Change domain of scale with inputfields

--- visualisation-tool/src/components/timeline/TimeScale.vue
+++ visualisation-tool/src/components/timeline/TimeScale.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-75 timelinecontainer">
     <div class="float-left col">
-      <input type="number" id="startscale">
-      <input type="number" id="endscale">
+      <input type="number" id="startscale" @change="onfieldschange" value="0">
+      <input type="number" id="endscale" @change="onfieldschange" value="100">
     </div>
     <div id="timeline" class="float-left w-100"></div>
   </div>
@@ -13,6 +13,14 @@ import * as d3 from 'd3';
 import { svg } from 'd3';
 export default {
   name: "timescale",
+  data() {
+    return {
+      timeaxis: '',
+      timescale: '',
+      gaxis: '',
+      zoom: '',
+    }
+  },
   mounted() {
     let svgcont = d3.select('#timeline').append('svg')
     .attr('width', '100%')
@@ -21,20 +29,30 @@ export default {
     .attr('id','timelinesvg')
     let width = document.getElementById("timeline")!.clientWidth - 30
 
-    let xscale = d3.scaleLinear().range([0, width]).domain([0,100])
-    let axis = d3.axisBottom(xscale)
+    this.timescale = d3.scaleLinear().range([0, width]).domain([0,100])
+    this.timeaxis = d3.axisBottom(this.timescale)
 
-    let zoom = d3.zoom()
+    this.zoom = d3.zoom()
       .scaleExtent([1, 40])
-      .translateExtent([[-100, -100], [width + 90, 500]])
-      .on('zoom', zoomed)
+      .translateExtent([[-10,0], [width + 90, 500]])
+      .on('zoom', this.zoomed)
 
-    let gx = svgcont.append("g")
-      .call(axis)
-    svgcont.call(zoom)
+    this.gaxis = svgcont.append("g")
+      .attr("class", "timeaxis")
+      .call(this.timeaxis)
+    svgcont.call(this.zoom)
+  },
+  methods: {
+    onfieldschange: function (){
+      let startscale = parseInt((<HTMLInputElement> document.getElementById("startscale")).value)
+      let endscale = parseInt((<HTMLInputElement> document.getElementById("endscale")).value)
+      
+      this.timescale.domain([startscale, endscale])
 
-    function zoomed () {
-      gx.call(axis.scale(d3.event.transform.rescaleX(xscale)))
+      d3.select(".timeaxis").call(this.zoom.transform, d3.zoomIdentity).call(this.timeaxis)
+    },
+    zoomed: function(){
+      this.gaxis.call(this.timeaxis.scale(d3.event.transform.rescaleX(this.timescale)))
     }
   }
 }
@@ -43,13 +61,13 @@ export default {
 <style>
 
 #startscale{
-  width: 40px;
+  width: 60px;
   height: 20px;
   float: left;
 }
 
 #endscale{
-  width: 40px;
+  width: 60px;
   height: 20px;
   float: right;
 }
