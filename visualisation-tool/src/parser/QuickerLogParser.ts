14b16616a3e6e3159e1bb50b17fcf67a884989f1
jonas reynders <reynders@live.be>
2018-08-11 17:17:53 +0200

    Parse Time delta of packet

--- visualisation-tool/src/parser/QuickerLogParser.ts
+++ visualisation-tool/src/parser/QuickerLogParser.ts
@@ -2,7 +2,7 @@ import Parser from "./parser"
 import {
     Trace, QuicConnection, QuicPacket, Header, LongHeader, ShortHeader,
     Payload, Frame, Padding, Rst_Stream, Application_Close, Max_Data, Max_Stream_Data, Max_Stream_Id, Ping, Blocked, Stream_Blocked, 
-    Stream_Id_Blocked, New_Connection_Id, Stop_Sending, Ack, Path_Challenge, Path_Response, Stream, Connection_Close, AckBlock, ServerInfo
+    Stream_Id_Blocked, New_Connection_Id, Stop_Sending, Ack, Path_Challenge, Path_Response, Stream, Connection_Close, AckBlock, ServerInfo, ConnectionInfo
 } from "../data/quic"
 
 
@@ -11,12 +11,14 @@ export class QuickerLogParser extends Parser{
         let trace = this.createTraceObject(name)
         tracefile = this.removeEscapeCharacters(tracefile)
         let packets = this.divideTextByPackets(tracefile)
+
+        this.parsePackets(packets)
         
         return trace
     }
 
     private removeEscapeCharacters(tracefile: string): string{
-        let filteredfile = tracefile.replace(/\[.../g, "");
+        let filteredfile = tracefile.replace(/\[.{2,3}/g, "");
         return filteredfile
     }
 
@@ -39,4 +41,44 @@ export class QuickerLogParser extends Parser{
         return trace
     }
 
+    private parsePackets(packets_string: Array<string>){
+        let starttime = this.getTime(packets_string[0].split('\n')[0])
+        packets_string.forEach((packet) => {
+            this.parsePacket(packet, starttime)
+        })
+    }
+
+    private getTime(timestamp: string): number{
+        let starttime_string = timestamp.split('T')[1]
+
+        return this.converTimeToMs(starttime_string)
+    }
+
+    private converTimeToMs(timestamp: string): number{
+        let splittime = timestamp.split(':')
+        let seconds = splittime[2].split('.')
+        let time = parseFloat(seconds[1])
+        time += parseFloat(seconds[0]) * 1000
+        time += parseFloat(splittime[1]) * 60 * 1000
+        time += parseFloat(splittime[0]) * 60 * 60 * 1000
+
+        return time
+    }
+
+    private parsePacket(packet_string: string, starttime: number){
+        let splitstring = packet_string.split('\n')
+        let conn_info = this.parseConnectionInfo(splitstring[0], starttime)
+    }
+
+    private parseConnectionInfo(conninfo_string: string, starttime: number): ConnectionInfo{
+        let conn_info: ConnectionInfo = {
+            src_ip_address: '',
+            src_port_number: -1,
+            dst_ip_address: '',
+            dst_port_number: -1,
+            time_delta: this.getTime(conninfo_string) - starttime
+        }
+
+        return conn_info
+    }
 }
