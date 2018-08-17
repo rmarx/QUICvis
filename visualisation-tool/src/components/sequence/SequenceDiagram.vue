5931f2ea3e50012368e017c305b881d2227c2cce
jonas reynders <reynders@live.be>
2018-08-17 20:13:08 +0200

    Show extra server info on the sides

--- visualisation-tool/src/components/sequence/SequenceDiagram.vue
+++ visualisation-tool/src/components/sequence/SequenceDiagram.vue
@@ -1,10 +1,18 @@
 <template>
-    <svg id="sequencediagram" v-if="validfiles" v-bind:height="getLargestTime + 'px'">
-        <line x1="150" y1="-100" x2="150" v-bind:y2="(getLargestTime + 100)"  stroke="black"/>
-        <line x1="850" y1="-100" x2="850" v-bind:y2="(getLargestTime + 100)"  stroke="black"/>
-        <SequenceArrow v-for="(packets, index) in sequencepackets" :packet_conn1="packets.packet_conn1" 
-        :packet_conn2="packets.packet_conn2" :baseheight="(index * margin)" :start_time="packets.start_time"/>
-    </svg>
+    <div v-if="validfiles" id="seq-diagramcontainer">
+        <svg id="sequencediagram" v-if="validfiles" v-bind:height="getLargestTime + 'px'">
+            <line x1="150" y1="-100" x2="150" v-bind:y2="(getLargestTime + 100)"  stroke="black"/>
+            <line x1="850" y1="-100" x2="850" v-bind:y2="(getLargestTime + 100)"  stroke="black"/>
+            <SequenceArrow v-for="(packets, index) in sequencepackets" :packet_conn1="packets.packet_conn1" 
+            :packet_conn2="packets.packet_conn2" :baseheight="(index * margin)" :start_time="packets.start_time"/>
+        </svg>
+        <div v-for="(packets, index) in sequencepackets">
+        <ServerInfo  :packet_conn="packets.packet_conn1" :baseheight="(index * margin)" :start_time="packets.start_time" 
+            :isclient="true" :_2filesused="false"/>
+        <ServerInfo :packet_conn="packets.packet_conn2" :baseheight="(index * margin)" :start_time="packets.start_time" 
+            :isclient="false" :_2filesused="true"/>
+        </div>
+    </div>
     <div id="nodiagram" v-else>
         Current file selection is invalid
     </div>
@@ -12,6 +20,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import SequenceArrow from './SequenceArrow.vue'
+import ServerInfo from './ServerInfo.vue';
 export default {
   name: "SequenceDiagram",
   data() {
@@ -31,18 +40,24 @@ export default {
       },
   },
   components: {
-      SequenceArrow
+      SequenceArrow,
+      ServerInfo
   }
 }
 </script>
 
 <style>
 #sequencediagram{
+    width: 1000px;
+    overflow: visible;
+}
+
+#seq-diagramcontainer{
     margin-left: 300px;
     margin-top: 100px;
     margin-bottom: 100px;
-    width: 1000px;
-    overflow: visible;
+    width: 1300px;
+    position: relative;
 }
 
 #nodiagram{
---
+++ visualisation-tool/src/components/sequence/ServerInfo.vue
@@ -0,0 +1,79 @@
+<template>
+    <div v-if="packet_conn !== null && packet_conn.serverinfo !== null" class="serverinfocontainer" :style="{top: (ytranslate + baseheight - 20) + 'px', left: lefttrans + 'px'}">
+        <button v-if="!expanded" class="btn btn-sm btn-primary position-absolute" @click="toggleSeverInfo()" :style="{left: buttontrans +'px'}">+</button>
+        <button v-if="expanded" class="btn btn-sm position-absolute" @click="toggleSeverInfo()" :style="{left: buttontrans +'px'}">-</button>
+        <div class="collapse">
+        <div v-for="info in packet_conn.serverinfo">
+            <div class="float-left">
+                {{info.infotype}}:
+            </div>
+            <div class="float-left">
+                {{info.infocontent}}
+            </div>
+            <br />
+        </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default {
+    name: "ServerInfo",
+    props: ['packet_conn', 'baseheight', 'isclient', 'start_time', '_2filesused'],
+    data() {
+        return {
+            expanded: false
+        }
+    },
+    computed: {
+        scale() {
+            return this.$store.state.sequencesettings.getTimeScale()
+        },
+        rtt_amount(){
+            return this.$store.state.sequencesettings.get1filertt()
+        },
+        ytranslate(){
+            if (this._2filesused) {
+                let t_p1 = parseFloat(this.packet_conn.connectioninfo.time_delta)
+                let t_p2 = parseFloat(this.packet_conn.connectioninfo.time_delta) + ((this.rtt_amount / 2) / 1000)
+
+                if (this.isclient)
+                    return t_p1 * 1000 * this.scale
+                else
+                    return (t_p2 * 1000 + 200)  * this.scale
+            }
+            if (this.start_time > 0)
+                return parseFloat(this.start_time) * this.scale
+            else
+                return parseFloat(this.packet_conn.connectioninfo.time_delta) * 1000 * this.scale
+        },
+        lefttrans(){
+            if (this.isclient)
+                return -250
+            else
+                return 1000
+        },
+        buttontrans(){
+            if (this.isclient)
+                return 270
+            else
+                return -60       
+        }
+    },
+    methods: {
+        toggleSeverInfo(){
+            let serverinfo = ( <HTMLElement> this.$el).children[1]
+            if (serverinfo.classList.contains('show'))
+                serverinfo.classList.remove('show')
+            else
+                serverinfo.classList.add('show')
+            this.expanded = !this.expanded
+        }
+    },
+}
+</script>
+<style>
+.serverinfocontainer{
+    position: absolute;
+}
+</style>
